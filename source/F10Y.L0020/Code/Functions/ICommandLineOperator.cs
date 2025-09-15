42ef28893ba850c70c4e469f7d3a45d78c0d27ba
davidcoats <david@getsafetycone.com>
2025-09-15 10:44:52 -0700

    Interim commit.

--- source/F10Y.L0020/Code/Functions/ICommandLineOperator.cs
+++ source/F10Y.L0020/Code/Functions/ICommandLineOperator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 
 using F10Y.T0002;
+using F10Y.T0011;
 
 
 namespace F10Y.L0020
@@ -11,13 +12,31 @@ namespace F10Y.L0020
     public partial interface ICommandLineOperator :
         L0000.ICommandLineOperator
     {
-        public new CommandResult Run_Synchronous(
+#pragma warning disable IDE1006 // Naming Styles
+
+        [Ignore]
+        L0000.ICommandLineOperator _L0000 => L0000.CommandLineOperator.Instance;
+
+#pragma warning restore IDE1006 // Naming Styles
+
+
+        CommandResult Run_Synchronous(
             string command,
             string arguments,
-            string workingDirectory)
+            string workingDirectory,
+            DataReceivedEventHandler outputHandler,
+            DataReceivedEventHandler errorHandler)
         {
             var outputLines_Accumulator = new List<string>();
             var errorLines_Accumulator = new List<string>();
+
+            var outputHandler_Aggregate = Instances.ProcessOperator.Get_DataReceivedEventHandler_Aggregate(
+                outputHandler,
+                Instances.ProcessOperator.Get_DataReceivedEventHandler(outputLines_Accumulator));
+
+            var errorHandler_Aggregate = Instances.ProcessOperator.Get_DataReceivedEventHandler_Aggregate(
+                errorHandler,
+                Instances.ProcessOperator.Get_DataReceivedEventHandler(errorLines_Accumulator));
 
             var exitCode = Instances.StopwatchOperator.Measure_Duration(
                 () =>
@@ -26,8 +45,8 @@ namespace F10Y.L0020
                         command,
                         arguments,
                         workingDirectory,
-                        Instances.ProcessOperator.Get_DataReceivedEventHandler(outputLines_Accumulator),
-                        Instances.ProcessOperator.Get_DataReceivedEventHandler(errorLines_Accumulator));
+                        outputHandler_Aggregate,
+                        errorHandler_Aggregate);
 
                     return exitCode;
                 },
@@ -42,7 +61,53 @@ namespace F10Y.L0020
             return output;
         }
 
-        public new CommandResult Run_Synchronous(
+        CommandResult Run_Synchronous(
+            string command,
+            string arguments,
+            DataReceivedEventHandler outputHandler,
+            DataReceivedEventHandler errorHandler)
+            => this.Run_Synchronous(
+                command,
+                arguments,
+                Instances.Values.WorkingDirectory_Default,
+                outputHandler,
+                errorHandler);
+
+        new CommandResult Run_Synchronous(
+            string command,
+            string arguments,
+            string workingDirectory)
+        {
+            var outputLines_Accumulator = new List<string>();
+            var errorLines_Accumulator = new List<string>();
+
+            var outputHandler = Instances.ProcessOperator.Get_DataReceivedEventHandler(outputLines_Accumulator);
+            var errorHandler = Instances.ProcessOperator.Get_DataReceivedEventHandler(errorLines_Accumulator);
+
+            var exitCode = Instances.StopwatchOperator.Measure_Duration(
+                () =>
+                {
+                    var exitCode = Instances.ProcessOperator.Run_Synchronous(
+                        command,
+                        arguments,
+                        workingDirectory,
+                        outputHandler,
+                        errorHandler);
+
+                    return exitCode;
+                },
+                out var duration);
+
+            var output = Instances.CommandResultOperator.Get_From(
+                outputLines_Accumulator.ToArray(),
+                errorLines_Accumulator.ToArray(),
+                exitCode,
+                duration);
+
+            return output;
+        }
+
+        new CommandResult Run_Synchronous(
             string command,
             string arguments)
             => this.Run_Synchronous(
@@ -50,10 +115,18 @@ namespace F10Y.L0020
                 arguments,
                 Instances.Values.WorkingDirectory_Default);
 
-        public CommandResult Run_Synchronous(CommandInvocation commandInvocation)
+        new CommandResult Run_Synchronous(string command)
+            => this.Run_Synchronous(
+                command,
+                Instances.Values.EmptyCommandArguments);
+
+        CommandResult Run_Synchronous(CommandInvocation commandInvocation)
         {
             var outputLines_Accumulator = new List<string>();
             var errorLines_Accumulator = new List<string>();
+
+            var outputHandler = Instances.ProcessOperator.Get_DataReceivedEventHandler(outputLines_Accumulator);
+            var errorHandler = Instances.ProcessOperator.Get_DataReceivedEventHandler(errorLines_Accumulator);
 
             var exitCode = Instances.StopwatchOperator.Measure_Duration(
                 () =>
@@ -62,8 +135,8 @@ namespace F10Y.L0020
                         commandInvocation.Command,
                         commandInvocation.Arguments,
                         commandInvocation.WorkingDirectory,
-                        Instances.ProcessOperator.Get_DataReceivedEventHandler(outputLines_Accumulator),
-                        Instances.ProcessOperator.Get_DataReceivedEventHandler(errorLines_Accumulator));
+                        outputHandler,
+                        errorHandler);
 
                     return exitCode;
                 },
@@ -78,7 +151,7 @@ namespace F10Y.L0020
             return output;
         }
 
-        public N001.CommandResult Run_Synchronous(CommandInvocation commandInvocation,
+        N001.CommandResult Run_Synchronous(CommandInvocation commandInvocation,
             DataReceivedEventHandler output_Handler)
         {
             var exitCode = Instances.StopwatchOperator.Measure_Duration(
